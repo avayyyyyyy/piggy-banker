71032d22b6e00cfe7d9037e0eab1dff6f7b47bff
avayyyyyyy <jabhi465@gmail.com>
2024-08-15 14:43:51 +0530

    feat(layout): add custom web component 'widget-web-component' to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,19 @@ export const metadata: Metadata = {
 
 getTransactions();
 
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "widget-web-component": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        projectid: string;
+      };
+    }
+  }
+}
+
 export default async function RootLayout({
   children,
 }: Readonly<{
@@ -31,9 +44,16 @@ export default async function RootLayout({
           {children}
           {/* <ReactQueryDevtools initialIsOpen={false} /> */}
         </ThemeProvider>
-
         <Toaster closeButton expand richColors position="bottom-right" />
         <Footer />
+        <div style={{ position: "fixed", bottom: "50px", right: "20px" }}>
+          <widget-web-component projectid="clztyubg70001ycxvkeeleplh"></widget-web-component>
+
+          <script
+            async
+            src="https://opinify-widget-w24d.vercel.app/widget.umd.js"
+          ></script>
+        </div>
       </body>
     </html>
   );
